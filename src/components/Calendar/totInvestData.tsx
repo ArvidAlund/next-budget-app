4a01358826c8c899062cf2252815f5616084c984
Arvid <arvid.alund@gmail.com>
2025-10-27 16:58:38 +0100

    function: see if has invested the amout in avanza

--- src/components/Calendar/totInvestData.tsx
+++ src/components/Calendar/totInvestData.tsx
@@ -2,21 +2,43 @@ import { useEffect, useState } from "react";
 import calcInvestment from "@/app/lib/calcInvestment";
 import { Container } from "../ui/container";
 import { formatCurrency } from "@/app/lib/formatcurrency";
+import supabase, { supabaseUserID } from "@/app/lib/supabaseClient";
 
 export default function TotInvestData(){
     const [investmentAmount, setInvestmentAmount] = useState<number>();
+    const [hasInvested, setHasInvested] = useState(false);
 
     useEffect(()=>{
         const getInvestmentAmount = async ()=>{
             const result: number = await calcInvestment();
             if (result === 0) return console.error("Något gick fel försök igen senare");
             setInvestmentAmount(result);
+
+            getAvanzaInvestment(result);
         }
+
+        const getAvanzaInvestment = async (amount: number) => {
+            const { data, error } = await supabase
+                .from('transaction')
+                .select('*')
+                .eq('type', 'utgift')
+                .eq('description', 'Avanza')
+                .eq('amount', amount)
+                .eq('category', 'sparande');
+
+            if (error) {
+                console.error("Supabase error:", error);
+                setHasInvested(false);
+                return;
+            }
+
+            setHasInvested(data && data.length > 0);
+        };
 
         getInvestmentAmount()
     },[])
 
-    return (investmentAmount ?<Container sizeClass="w-full h-fit flex flex-col" colorClass="bg-white">
+    return (investmentAmount ?<Container sizeClass="w-full h-fit flex flex-col" colorClass={hasInvested ? "bg-green-500" : "bg-red-500"}>
         <div className="text-black text-center font-bold">
             <h1>Att invstera denna månad</h1>
             <h3>{investmentAmount ? `${formatCurrency(investmentAmount)} kr`:"Laddar..."}</h3>
