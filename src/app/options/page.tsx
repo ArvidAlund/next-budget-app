e215de9726ea0a6a8bbaffd33339ddfcc0acb2ff
Arvid <arvid.alund@gmail.com>
2025-11-19 12:50:53 +0100

    function: options list created

--- src/app/options/page.tsx
+++ src/app/options/page.tsx
@@ -1,4 +1,8 @@
+"use client";
 
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 const optionsList = [
   { id: 'general', name: 'Allmänt' },
   { id: 'appearance', name: 'Utseende' },
@@ -13,14 +17,18 @@ const optionsList = [
 
 
 export default function OptionsPage() {
+    const [selectedOption, setSelectedOption] = useState<string | null>(null);
   return (
     <>
         <main className="grid sm:grid-cols-2">
             <div>
                 {optionsList.map(option => (
-                    <div key={option.id} className="p-4 border-b bg-primary-400 text-secondary">
-                        <h2 className="text-lg font-semibold">{option.name}</h2>
-                        <p className="text-sm text-gray-600">Inställningar för {option.name.toLowerCase()}.</p>
+                    <div key={option.id} className="p-4 border-b bg-primary-400 text-secondary flex items-center justify-between hover:bg-primary-300 cursor-pointer transition-all duration-300" onClick={() => setSelectedOption(option.id)}>
+                        <div>
+                            <h2 className="text-lg font-semibold">{option.name}</h2>
+                            <p className="text-sm text-gray-600">Inställningar för {option.name.toLowerCase()}.</p>
+                        </div>
+                        <FontAwesomeIcon icon={faChevronRight} className={`${selectedOption === option.id ? "rotate-180" : "rotate-0"} transition-all duration-300`}/>
                     </div>
                 ))}
             </div>
