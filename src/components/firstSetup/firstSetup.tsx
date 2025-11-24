da644d90d1205e9b8d09a416411d39c4c7e667b3
Arvid <arvid.alund@gmail.com>
2025-11-24 10:12:17 +0100

    feature: add inital amount function added

--- src/components/firstSetup/firstSetup.tsx
+++ src/components/firstSetup/firstSetup.tsx
@@ -6,7 +6,7 @@ import BudgetTypeOption from "../options/optionFunctions/budgetType";
 import AddExpense from "./addExpense";
 import { useUnsavedChanges } from "@/hooks/useUnsavedChanges";
 import { Button } from "../ui/button";
-import { onEvent } from "@/app/lib/eventbus";
+import { onEvent, emitEvent } from "@/app/lib/eventbus";
 
 type Income = { day: string; amount: number };
 type IncomeData = {
@@ -21,6 +21,8 @@ export default function FirstSetup() {
   const { unsavedChanges, setSaveChanges } = useUnsavedChanges();
   const [incomeList, setIncomeList] = useState<IncomeData | null>(null);
   const [expenseList, setExpenseList] = useState<Expense[]| null>(null)
+  const [startAmount, setStartAmount] = useState<number>()
+  const totSteps: number = 5;
 
   useEffect(() => {
     const unsubscribe = onEvent("Income-data", (data: IncomeData) => {
@@ -31,11 +33,19 @@ export default function FirstSetup() {
       setExpenseList(data.expense);
     });
 
+    const unsubscribeStart = onEvent("Start-Income", (data: number) => {setStartAmount(data)})
+
     return () => {
         unsubscribe();
         unsubscribeExpense();
+        unsubscribeStart();
     }
   }, []);
+
+  useEffect(()=>{
+    if (stage <= totSteps) return
+    console.log("yay")
+  },[stage])
 
   const renderStage = () => {
     switch (stage) {
@@ -47,22 +57,24 @@ export default function FirstSetup() {
             return <AddIncome />;
         case 4:
             return <AddExpense />;
+        case 5:
+            return <AddInitalAmount/>;
       default:
         return null;
     }
   };
 
   return (
-    <main className="fixed top-0 left-0 z-200 w-full h-full bg-primary flex items-center justify-center overflow-x-scroll">
+    <main className="fixed top-0 left-0 z-200 w-full h-full bg-primary flex items-center justify-center">
         <div className="border min-w-1/2 bg-secondary">
             {renderStage()}
-            <div className="mt-2 text-center">
-                <Button className="m-2 px-8 text-lg" onClick={() => setStage(stage + 1)}>Nästa steg</Button>
-                <p>Steg {stage} av 7</p>
+            <div className={`mt-2 text-center ${stage === totSteps+1 ? "hidden" : "block"}`}>
+                <Button className="m-2 px-8 text-lg" onClick={() => setStage(stage + 1)}>{totSteps === stage ? "Bekräfta och starta" : "Nästa steg"}</Button>
+                <p>Steg {stage} av {totSteps}</p>
                 <div className="h-2 w-[90%] m-auto rounded-full overflow-hidden bg-gray-200">
                     <div
                         className="bg-green-500 h-full transition-all duration-300"
-                        style={{ width: `${(stage / 7) * 100}%` }}
+                        style={{ width: `${(stage / totSteps) * 100}%` }}
                     />
                 </div>
             </div>
