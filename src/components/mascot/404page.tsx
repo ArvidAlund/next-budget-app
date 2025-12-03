80370e69d11b1128cca53232904a7156d675b667
Arvid <arvid.alund@gmail.com>
2025-12-03 10:57:13 +0100

    update: added motion to 404 page

--- src/components/mascot/404page.tsx
+++ src/components/mascot/404page.tsx
@@ -12,7 +12,12 @@ import Image from "next/image";
  */
 export default function Mascot404Page() {
   const [isVideoReady, setIsVideoReady] = useState(false);
-  const canShowVideo = false;
+  const canShowVideo = true;
+
+  const onLoadedData = () => {
+    console.log("Video loaded!");
+    setIsVideoReady(true);
+  };
 
   return (
     <div className="w-full h-full flex items-center justify-center fixed top-1/2 left-1/2 z-100 -translate-x-1/2 -translate-y-1/2 user-noselect pointer-events-none">
@@ -30,16 +35,17 @@ export default function Mascot404Page() {
       {/* Videon laddas alltid men är osynlig tills den är redo */}
       {canShowVideo && (
         <video
-            src="/mascot/video/confused.mp4"
-            autoPlay
-            loop
-            muted
-            playsInline
-            className={`object-contain mix-blend-multiply ${!isVideoReady ? "hidden" : "block"} aspect-video`}
-            width={1024}
-            onCanPlayThrough={() => setIsVideoReady(true)}
+          src="/mascot/video/confused.webm"
+          autoPlay
+          loop
+          muted
+          playsInline
+          className={`object-contain ${!isVideoReady ? "hidden" : "block"} aspect-video`}
+          width={1024}
+          onLoadedData={onLoadedData}
         />
       )}
+
       <div className="bg-neutral-700/30 blur-sm w-70 h-6 rounded-[100%] absolute top-[69%] left-[52%] -translate-y-1/2 -translate-x-1/2 -z-10"></div>
     </div>
   );
