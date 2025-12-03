55bb246f5291890be67335674a6f1aa92874df5f
Arvid <arvid.alund@gmail.com>
2025-12-03 19:38:23 +0100

    ui: removed styling classes

--- src/components/home/investment.tsx
+++ src/components/home/investment.tsx
@@ -49,7 +49,7 @@ export default function Investment({ className }: InvestmentProps) {
     return (
     <section className={`${className} w-full h-full`}>
           <Container>
-            <h3 className="text-2xl font-semibold text-white mb-6 tracking-wide">Investera</h3>
+            <h3 className="text-white mb-6 tracking-wide">Investera</h3>
 
             <div
               className={`w-full max-w-md px-6 py-4 flex flex-col items-center justify-center rounded-xl border ${
