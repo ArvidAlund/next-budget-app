68acda563ac4ea593ec12b5a1285ff7d402186e3
Arvid <arvid.alund@gmail.com>
2025-12-04 11:23:01 +0100

    ux/ui: summary component scaling fix

--- src/components/home/summary.tsx
+++ src/components/home/summary.tsx
@@ -107,21 +107,21 @@ export default function Summary({ className }: SummaryProps) {
         {loaded ? (
           <>
             <h1 className="my-4">{formatCurrency(valueData.total)} kr</h1>
-            <div className="grid grid-cols-2 gap-4 h-15">
-              <div className="bg-green-900/10 border border-green-500/50 rounded-lg p-4 shadow-sm hover:shadow-md transition-shadow duration-300 h-full w-full">
+            <div className="grid grid-cols-2 gap-4">
+              <div className="bg-green-900/10 border border-green-500/50 rounded-lg p-4 shadow-sm hover:shadow-md transition-shadow duration-300 h-full w-full text-xs lg:text-lg lg:shadow-lg">
                 <div className="flex items-center gap-2">
                   <ArrowUpRight className="w-5 h-5 text-green-400" />
-                  <span className="text-green-400 md:text-lg font-bold">
+                  <span className="text-green-400 font-bold">
                     {formatCurrency(valueData.income)} kr
                   </span>
                 </div>
                 <p className="text-sm text-neutral-300 mt-1">Inkomster</p>
               </div>
 
-              <div className="bg-red-900/10 border border-red-500/50 rounded-lg p-4 shadow-sm hover:shadow-md transition-shadow duration-300 h-full w-full">
+              <div className="bg-red-900/10 border border-red-500/50 rounded-lg p-4 text-xs lg:text-lg lg:shadow-lg hover:shadow-md transition-shadow duration-300 h-full w-full">
                 <div className="flex items-center gap-2">
                   <ArrowDownLeft className="w-5 h-5 text-red-400" />
-                  <span className="text-red-400 md:text-lg font-bold">
+                  <span className="text-red-400 font-bold">
                     {formatCurrency(valueData.expense)} kr
                   </span>
                 </div>
@@ -130,7 +130,7 @@ export default function Summary({ className }: SummaryProps) {
             </div>
 
             {/* Snabbstatistik */}
-            <section className="mt-6">
+            <section className="">
               <h4 className="text-md font-semibold mb-2">Snabbstatistik</h4>
               <div className="flex flex-wrap gap-3">
                 <Statistic Icon={ArrowLeftRight} text={`${transactions.length} antal transaktioner`} type="neutral" />
