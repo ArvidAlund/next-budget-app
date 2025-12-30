897f59577faa94c082eb8be85b2e81af90642a89
Arvid <arvid.alund@gmail.com>
2025-12-30 23:03:25 +0100

    bugfix: one more potential issue resolved

--- src/components/ui/balanceAnimation.tsx
+++ src/components/ui/balanceAnimation.tsx
@@ -6,15 +6,20 @@ const BalanceAnimation = ({start = 0, end, duration = 1000} : {start?: number, e
 
     useEffect(() => {
         let startTimestamp: number | null = null;
+        let animationId: number;
         const step = (timestamp: number) => {
             if (!startTimestamp) startTimestamp = timestamp;
             const progress = Math.min((timestamp - startTimestamp) / duration, 1);
             setBalance(Math.floor(progress * (end - start) + start));
             if (progress < 1) {
-                window.requestAnimationFrame(step);
+                animationId = window.requestAnimationFrame(step);
             }
         };
-        window.requestAnimationFrame(step);
+        animationId = window.requestAnimationFrame(step);
+
+        return () => {
+            window.cancelAnimationFrame(animationId);
+        };
     }, [start, end, duration]);
 
     return (
