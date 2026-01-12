8fe375a3b004b2d7544bb186c34b100aa6f8891f
Arvid <arvid.alund@gmail.com>
2026-01-12 18:28:51 +0100

    text: bio updated for Arvid Ålund to be a bit longer and paint a better picture

--- src/components/about/personal.tsx
+++ src/components/about/personal.tsx
@@ -4,7 +4,7 @@ const Personal = () => {
         <h2>Vårt team</h2>
         <hr className="mb-4 border-neutral-700" />
         <ul className="p-4">
-            <li className="mb-8 grid grid-cols-2 gap-4 items-center">
+            <li className="mb-8 grid grid-cols-2 gap-4 items-center h-full">
                 <img
                 src="https://avatars.githubusercontent.com/u/64034931?v=4"
                 alt="Arvid Ålund"
@@ -12,10 +12,10 @@ const Personal = () => {
                 height={300}
                 className="rounded-full mx-auto"
                 />
-                <div className="flex flex-col justify-center">
+                <div className="flex flex-col justify-center h-full">
                     <h2 className="font-bold">Arvid Ålund</h2>
-                    <p className="mt-2">Roll: Ägare, Utvecklare</p>
-                    <p className="mt-2">Var i behov av en budgetapp för att bättre hantera mina utgifter. <br/>Så jag byggde en.</p>
+                    <p className="text-sm font-medium">Ägare, Utvecklare</p>
+                    <p className="mt-6">Jag behövde ett bättre sätt att förstå vart mina pengar faktiskt tog vägen. Budgetapparna som fanns kändes antingen för krångliga, för begränsade eller helt enkelt inte byggda för hur jag tänker. <br/><br/>Så jag gjorde det enda rimliga: jag byggde en egen.</p>
                 </div>
             </li>
         </ul>
