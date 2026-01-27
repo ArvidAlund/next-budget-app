821713db7b23634a86f69c91ed3697056161b272
Arvid <arvid.alund@gmail.com>
2026-01-27 16:53:41 +0100

    option: transaction_decimals option implemented

--- src/app/lib/formatcurrency.ts
+++ src/app/lib/formatcurrency.ts
@@ -7,31 +7,32 @@
 export function formatCurrency(amount: number) {
   const options = localStorage.getItem("user_options");
   const formatSetting = options ? JSON.parse(options).thousand_separator : "decimal";
+  const transactionDecimalSetting : boolean = options ? JSON.parse(options).transaction_decimals : true;
 
   if (formatSetting === "space") {
     return amount.toLocaleString("sv-SE", {
       style: "decimal",
       minimumFractionDigits: 0,
-      maximumFractionDigits: 2,
+      maximumFractionDigits: transactionDecimalSetting ? 2 : 0,
     }).replace(/\u00A0/g, ' '); // Replace non-breaking space with regular space
   }
   if (formatSetting === "comma") {
     return amount.toLocaleString("de-DE", {
       style: "decimal",
       minimumFractionDigits: 0,
-      maximumFractionDigits: 2,
+      maximumFractionDigits: transactionDecimalSetting ? 2 : 0,
     });
   }
 
   if (formatSetting === "none") {
-    return amount.toFixed(0);
+    return amount.toFixed(transactionDecimalSetting ? 2 : 0);
   }
 
   if (formatSetting === "dot") {
     return amount.toLocaleString("en-IN", {
       style: "decimal",
       minimumFractionDigits: 0,
-      maximumFractionDigits: 2,
+      maximumFractionDigits: transactionDecimalSetting ? 2 : 0,
     });
   }
 }
